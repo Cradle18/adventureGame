5eb06f8a3b14598ef870fe626c9a5578896c006c
Tom Paul <t-paul@hotmail.co.uk>
2023-05-01 14:01:47 +0100

    scene.py

--- scene.py
+++ scene.py
@@ -1,13 +1,19 @@
+#scene class setup and any function that will be used inside the class or out.
+
+#---Imports---#
 import time
 
+
+#type writer effect, as if the words are being typed as they are presented
 def print_slow(text):
     for char in text:
-        print(char, end='', flush=True)
+        print(char, end='', flush=True) 
         time.sleep(0.09)
     print()
    
-
+#scene class
 class Scene:
+    directions = ["left", "right", "forward", "backward"]
     def __init__(self, name):
         self.name = name
 
@@ -31,22 +37,53 @@ class Welcome(Scene):
         
 
 class Part1(Scene):
+
     def print_part1(self):
         print_slow("The moon is bright and full above, the path before you lit, only by the moons reflection off your armor.")
         print_slow("You are surrounded by high tree's and thick bushes that flank ether side, blocking what you can see through the tree's, the forrest of HIGHRISE growls in the night.")
         print_slow(f"You follow a deralict road which leads towards nothing but darkness, gripping tight your {weapon}, ready for any surprise.")
         
 
-        print_slow(f"\nA noise ahead, a screech and a scratch, you ready your {weapon}, do you wish to investigate?(yes/no)")
+        print_slow(f"\nA noise ahead, a distant roar, you ready your {weapon} as the sound of crashing feet grows closer, do you wish to investigate?(yes/no)")
         inspect_ahead = input()
 
         if inspect_ahead == "yes":
-            print_slow(f"With one hand ready on your {weapon}, you move slowly towards the sound ahead, the scratching sound getting louder.")
-            print("\n'ROAR!'")
-            time.sleep(1)
-            print("\n'CRASH!'")
-            time.sleep(1)
-            print_slow("\nNow all you see is black! As you crash into a tree with a 'CRACK!'.")
-            print_slow("You lay slumbed, back against a broken a tree, a large shadow approaches over you and slings your lifeless body over it's shoulder and walks off back from where it came.")
+            captured = Captured("Captured")
+            captured.print_captured()
         else:
-            print("Dead")
+            lost = Lost("Lost")
+            lost.print_lost()
+            
+
+class Captured(Scene):
+    def print_captured(self):
+        print_slow(f"With one hand ready on your {weapon}, you move slowly towards the sound ahead, the thunderous clap hitting the ground getting louder!")
+        print("\n'ROAR!'")
+        time.sleep(1)
+        print("\n'CRASH!'")
+        time.sleep(1)
+        print_slow("\nNow all you see is black! As you crash into a tree with a 'CRACK!'.")
+        print_slow("You lay slumbed, back against a broken a tree, a large shadow approaches over you and slings your lifeless body over it's shoulder and walks off back from where it came.")
+
+class Lost(Scene):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def print_lost(self):
+        print_slow("You see a gap in the bush to your left and dive through it, landing safely with a role and take cover behind a tree.")
+        print_slow("You peer round and look back to the road, the ground shakes, as thunderous footsteeps from the path, pass you by.")
+        print_slow("You sigh with relief, knowing that the danger has moved on, as the sound of footsteep disappear of into the distance.")
+        print_slow("You turn around and realise that now all you can see is black, no light it peering through tree's, which direction do you wish to take?")
+        print("(Left, Right, Forwards, Backwards)")
+        direction = input()
+        if direction.lower() == self.directions[0]:
+            print("Yay!")
+        elif direction.lower() == self.directions[1]:
+            print("o no!")
+        elif direction.lower() == self.directions[2]:
+            print("dead")
+        elif direction.lower() == self.directions[3]:
+            print("trap")
+        else:
+            print("Invalid input!")
+            
