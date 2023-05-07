6c614d4720e0dac650efbd4b04f2451812bcf997
Tom Paul <t-paul@hotmail.co.uk>
2023-05-07 11:56:34 +0100

    finished skele fight 1 and started work on first puzzle

--- scene.py
+++ scene.py
@@ -59,13 +59,46 @@ class Part1(Scene):
 class Captured(Scene):
     def print_captured(self):
         print_slow(f"With one hand ready on your {new_user.weapon}, you move slowly towards the sound ahead, the thunderous clap hitting the ground getting louder!")
-        new_user.lose_health(5)
+        new_user.lose_health(25)
         print("\n'ROAR!'")
         time.sleep(1)
         print("\n'CRASH!'")
         time.sleep(1)
         print_slow("\nNow all you see is black! As you crash into a tree with a 'CRACK!'.")
         print_slow("You lay slumbed, back against a broken a tree, a large shadow approaches over you and slings your lifeless body over it's shoulder and walks off back from where it came.")
+        time.sleep(1)
+        print_slow("You awake feeling groggy, a sharp pain banging through your head, your whole body aches.")
+        print_slow("While slowly sitting up, you look around and bars of metal suround you, a chain wrapped around one of your legs.")
+        print_slow("Conclusion, you are locked in a cage. You sigh and begin to look around for any way out.")
+        print_slow("You notice a lock ahead and wonder if you can pick it! Do you wish to attempt your escape? (yes/no)")
+        escape_answer = input()
+        if escape_answer.lower() == "yes":
+            self.__puzzle_1(new_user)
+        else:
+            print_slow("You decide to roll over and acept your fate, after sometime you notice the smell of burning.")
+            print_slow("Suddenly the cage door crashes open and a ogre grabs you by the arm pulling you out of the cage.")
+            print_slow("He pulls with such a force but not enough to pull the chain out of the floor with you, you cry out in pain, as your leg tears away from your body and is left behind with the chain.")
+            print_slow("You scream out in agony as the orge take you towards the fire, atop sits a cauldron, you realise your mistake.")
+            new_user.is_dead()
+    
+    def __puzzle_1(self, user):
+        print_slow("You scramble your way over to lock and begin to look it over for any clues on how to unlock it")
+        print_slow("You notice strange markings on the side and a roller of numbers along the bottom.")
+        print_slow("\nII IV V VII | |1|2|3|4|\n")
+        guesses = 3
+        while guesses > 0:
+            user_answer = input("Whats your guess?: ")
+            if user_answer == "2457":
+                break
+            else:
+                guesses -= 1
+            if guesses == 0:
+                print_slow("A strange buzzing sound comes from inside the lock!")
+                time.sleep(0.5)
+                print("\nBANG!")
+                user.is_dead()
+        
+
 
 class Lost(Scene):
     def __init__(self, name):
@@ -79,16 +112,27 @@ class Lost(Scene):
         print("(Left, Right, Forwards, Backwards)")
         direction = input()
         if direction.lower() == self.directions[0] or direction.lower() == self.directions[1]:
-            print_slow("\nYou turn your head left and then right, you think you can see light, you turn and begin to walk forward.")
-            print_slow("After what seems like hours, a little moonlight begins to peer through the tree's ahead and you pick up the pace hoping to reach the path again.")
-            print_slow("A rustle from the bush ahead startles you, as two skeletons charge at you with there swords raised\n")
-            mons1 = Monster_Easy("Skulldugery")
-            mons2 = Monster_Easy("Skelebob")
-            battle(new_user, mons1, mons2)
+            self.__skeleton_fight_1(new_user)
         elif direction.lower() == self.directions[2]:
             print("trap")
         elif direction.lower() == self.directions[3]:
             print("back to road captured")
         else:
             print("Invalid input!")
+
+    def __skeleton_fight_1(self, user):
+            print_slow("\nYou turn your head left and then right, you think you can see light, you turn and begin to walk forward.")
+            print_slow("After what seems like hours, a little moonlight begins to peer through the tree's ahead and you pick up the pace hoping to reach the path again.")
+            print_slow("A rustle from the bush ahead startles you, as two skeletons charge at you with there swords raised\n")
+            mons1 = Monster_Easy("Skulldugery")
+            mons2 = Monster_Easy("Skelebob")
+            battle(user, mons1, mons2)
+            print_slow("\nSuccess! You have vanquished your foes, you take a moment to catch your breath and continue to walk ahead to rejoin the path.")
+            print_slow("You push through a small gap in the bushes, crawling on your hands and knees, you have made it! Time to pick up where you left off.")
+            print_slow("You continue to follow the path, walking at a faster pace than before, after a few minutes you begin to smell something.")
+            print_slow("You can smell the burning of flesh and you realise that you are walking right towards it!")
+            
+    
+    def __trap(self, user):
+        pass
             
